2cdbee06468aa802358ceb1da8c62783c9c57c09
jmaraya1229 <jessemaraya2013@gmail.com>
2022-01-06 22:00:45 -0800

    edited html and js

---
+++ apps.js
@@ -0,0 +1,11 @@
+const myQuestions = [
+    {
+        question: "What does HTML do?",
+        answers: { 
+            a: "Defines the content of web pages",
+            b: "Specifies the layout of web pages",
+            c: "Programs the behavior of web pages"
+        },
+        correctAnswer: "a"
+    },
+]
